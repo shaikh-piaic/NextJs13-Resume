45b3a9b9acce09ab7e07b1ddb66a81c8b1abda90
Hafiz Shaikh Abdul Sami <108601988+shaikh-piaic@users.noreply.github.com>
2023-03-24 00:16:02 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Skills from "./components/Skills";
-import Hireme from "./components/Hireme";
 import Project from "./components/Project";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
@@ -14,7 +13,6 @@ export default function Home() {
 			<Hero />
 			<About />
 			<Skills />
-			{/* <Hireme /> */}
 			<Project />
 			<Contact />
 			<Footer />
