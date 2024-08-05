adf8e18ddfcdadb22c4669fd4d68aae76bd0de9d
Hafiz Shaikh Abdul Sami <108601988+shaikh-piaic@users.noreply.github.com>
2024-08-06 01:05:08 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import GoogleAnalytics from "./components/GoogleAnalytics";
 import "./globals.css";
-
+import Script from "next/script";
 export const metadata = {
 	title: "Shaikh Abdul Sami Portfolio",
 	description: "Personal Portfolio",
@@ -17,15 +17,15 @@ export default function RootLayout({
 	return (
 		<html lang='en'>
 			<head>
-				<script
+				<Script
 					type='text/javascript'
-					src='https://vdbaa.com/pun.php?section=General&pt=6&pub=126596&ga=g'></script>
-				<script
+					src='https://vdbaa.com/pun.php?section=General&pt=6&pub=126596&ga=g'></Script>
+				<Script
 					type='text/javascript'
-					src='//pl23942191.highratecpm.com/86/75/b8/8675b85b91bf79de586b1db85257c2ba.js'></script>
-				<script
+					src='//pl23942191.highratecpm.com/86/75/b8/8675b85b91bf79de586b1db85257c2ba.js'></Script>
+				<Script
 					type='text/javascript'
-					src='//tuckbrows.com/86/75/b8/8675b85b91bf79de586b1db85257c2ba.js'></script>
+					src='//tuckbrows.com/86/75/b8/8675b85b91bf79de586b1db85257c2ba.js'></Script>
 				<GoogleAnalytics />
 			</head>
 			<body>{children}</body>
